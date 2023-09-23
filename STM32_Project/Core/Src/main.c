8b19146320ca9df99ad1108bfbeb437c7a90637b
MSI\nvtie <tien.ngo30082002@hcmut.edu.vn>
2023-09-24 00:10:05 +0700

    Exercise 8

--- STM32_Project/Core/Src/main.c
+++ STM32_Project/Core/Src/main.c
@@ -63,6 +63,21 @@ void clearAllClock(){
 	  HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, SET);
 	  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, SET);
 }
+
+void setNumberOnClock(int num){
+	  if(num==0) HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, RESET);
+	  else if(num==1) HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, RESET);
+	  else if(num==2) HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, RESET);
+	  else if(num==3) HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, RESET);
+	  else if(num==4) HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, RESET);
+	  else if(num==5) HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, RESET);
+	  else if(num==6) HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, RESET);
+	  else if(num==7) HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, RESET);
+	  else if(num==8) HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, RESET);
+	  else if(num==9) HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, RESET);
+	  else if(num==10) HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, RESET);
+	  else if(num==11) HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, RESET);
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -125,18 +140,8 @@ int main(void)
 		  sec = 0;
 	  }
 
-	  if(sec==0) HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, RESET);
-	  else if(sec==1) HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, RESET);
-	  else if(sec==2) HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, RESET);
-	  else if(sec==3) HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, RESET);
-	  else if(sec==4) HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, RESET);
-	  else if(sec==5) HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, RESET);
-	  else if(sec==6) HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, RESET);
-	  else if(sec==7) HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, RESET);
-	  else if(sec==8) HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, RESET);
-	  else if(sec==9) HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, RESET);
-	  else if(sec==10) HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, RESET);
-	  else if(sec>=11) HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, RESET);
+	  setNumberOnClock(sec);
+
 	  sec++;
 	  HAL_Delay(1000);
     /* USER CODE END WHILE */
